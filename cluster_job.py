03ce845ebeb35b0928f3cbd3883d4298990cd367
Matthew Collins <mcollins@acis.ufl.edu>
2016-04-10 20:23:35 -0400

    Generate figure

--- cluster_job.py
+++ cluster_job.py
@@ -4,11 +4,15 @@ import HTMLParser
 from pyspark import SparkContext, SQLContext
 from pyspark.sql.functions import udf, explode
 from pyspark.ml.feature import Tokenizer, StopWordsRemover
+import matplotlib as mpl
+mpl.use('Agg')
+import seaborn as sns
 
+# Create contexts
 sc = SparkContext(appName="SparkWorkshop")
 sqlContext = SQLContext(sc)
 
-
+# Set up user defined functions and object for transformations
 expression = re.compile(r'<.*?>')
 parser = HTMLParser.HTMLParser()
 
@@ -22,10 +26,9 @@ tokenizer = Tokenizer(inputCol="comment_clean", outputCol="words")
 stopWordsRemover = StopWordsRemover(inputCol="words", outputCol="tokens")
 
 # Load data
-#comments = sqlContext.read.json("hdfs://cloudera0.acis.ufl.edu:8020/user/mcollins/hacker_news_small.json")
 comments = sqlContext.read.json("data/hacker_news_small.json")
 
-# Calcualte tokens dataframe
+# Calcualte tokens dataframe as one pipeline
 tokens = stopWordsRemover.transform(
              tokenizer.transform(comments\
                  .withColumn("comment_clean", strip_tags_udf(comments["comment_text"]))\
@@ -38,6 +41,11 @@ tokens = stopWordsRemover.transform(
          .select("count")\
          .limit(1000)
 
+# Switch to Pandas
 tokens_pdf = tokens.toPandas()
 tokens_pdf["rank"] = range(1, tokens_pdf.shape[0] + 1)
 print(tokens_pdf.head())
+
+# Make a graph
+fig = sns.jointplot(x="rank", y="count", data=tokens_pdf)
+fig.savefig('temp.png')
